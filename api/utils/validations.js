179d1e6e6f0505e70c47750fa8bafadbd610e1e6
Franco Sebastian Muñoz <franco.mz92@hotmail.com>
2023-10-17 20:31:36 -0300

    API - Add security utilities

---
+++ api/utils/validations.js
@@ -0,0 +1,10 @@
+import { validationResult } from 'express-validator';
+
+export const validateResult = (req, res, next) => {
+  try {
+    validationResult(req).throw();
+    return next();
+  } catch (error) {
+    res.status(400).send({ errors: error.array() });
+  }
+};
